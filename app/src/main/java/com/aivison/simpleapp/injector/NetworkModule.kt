e745231580e4decf15ee030f2601dedf231a3533
Shailesh K Limbadiya <limbadiya.sk@gmail.com>
2024-04-04 19:37:20 +0530

    Update NetworkModule.kt

--- app/src/main/java/com/aivison/simpleapp/injector/NetworkModule.kt
+++ app/src/main/java/com/aivison/simpleapp/injector/NetworkModule.kt
@@ -16,9 +16,25 @@ class NetworkModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(context: Context, cacheDir: File): Retrofit {
+        val cacheSize = (5*1024*1024).toLong()
+        val cache = Cache(cacheDir, cacheSize)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .cache(cache)
+            .addInterceptor { chain ->
+                var request = chain.request()
+                request = if (hasNetwork(context)!!)
+                    request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                else
+                    request.newBuilder().header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7).build()
+                chain.proceed(request)
+            }
+            .build()
+
         return Retrofit.Builder()
-            .baseUrl(Constants.URL_BASE)
+            .client(okHttpClient)
+            .baseUrl("https://jsonplaceholder.typicode.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
@@ -27,5 +43,13 @@ class NetworkModule {
     @Singleton
     fun provideApi(retrofit: Retrofit): Api {
         return retrofit.create(Api::class.java)
+    }
+    fun hasNetwork(context: Context): Boolean? {
+        var isConnected: Boolean? = false // Initial Value
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        if (activeNetwork != null && activeNetwork.isConnected)
+            isConnected = true
+        return isConnected
     }
 }
